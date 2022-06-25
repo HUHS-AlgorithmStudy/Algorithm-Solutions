c7483ea558e8aed7752bffbf93ddf0c75ece8d17
KimGaeun0806 <greencloud11@hanyang.ac.kr>
2022-06-25 20:45:15 +0900

    week1 solved

--- WEEK1.js
+++ WEEK1.js
@@ -1,3 +1,4 @@
+// 최종 코드
 function solution(s) {
   let num = 0;
   for (let x of s) {
@@ -7,7 +8,7 @@ function solution(s) {
   return num === 0 ? true : false;
 }
 
-// 정확성 테스트는 통과했지만 효율성 테스트에서 실패한 코드
+// 정확성 테스트는 통과했지만 효율성 테스트에서 실패한 코드들
 function solution2(s) {
   while (s.includes('()')) s = s.split('()').join('');
   return s === '' ? true : false;
