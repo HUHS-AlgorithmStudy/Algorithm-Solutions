8b82130cdb7e87e20b610dbee488632f5824d022
HongSumin <googijnh4545@gmail.com>
2022-07-04 00:38:16 +0900

    miss code update

--- week2/12951_홍수민.js
+++ week2/12951_홍수민.js
@@ -9,7 +9,7 @@ function solution(s) {
     .split(" ")
     .map((s) => {
       let arr = s.split("");
-      if (arr[0] !== null) arr[0] = arr[0].toUpperCase();
+      if (arr[0] != null) arr[0] = arr[0].toUpperCase();
       return arr.join("");
     })
     .join(" ");
