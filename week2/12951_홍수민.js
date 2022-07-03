016b2ac1b3493da81b381cc4a101584325b81b51
HongSumin <googijnh4545@gmail.com>
2022-07-04 00:08:08 +0900

    week2 problem solved

---
+++ week2/12951_홍수민.js
@@ -0,0 +1,18 @@
+/*
+    프로그래머스 2단계 - JadenCase 문자열 만들기
+    https://programmers.co.kr/learn/courses/30/lessons/12951?language=javascript
+*/
+
+function solution(s) {
+  let string = s.toLocaleLowerCase();
+  let answer = string
+    .split(" ")
+    .map((s) => {
+      let arr = s.split("");
+      if (arr[0] !== null) arr[0] = arr[0].toUpperCase();
+      return arr.join("");
+    })
+    .join(" ");
+
+  return answer;
+}
