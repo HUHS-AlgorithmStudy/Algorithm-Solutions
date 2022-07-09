a7b7feb0ae565a97a16d0990373989bfaddef844
KimGaeun0806 <greencloud11@hanyang.ac.kr>
2022-07-10 02:48:13 +0900

    week3 solved

---
+++ KimGaeun/WEEK3.js
@@ -0,0 +1,11 @@
+// 최종 코드
+function solution(clothes) {
+  let numArr = [];
+  clothes
+    .map((a) => a[1])
+    .sort()
+    .forEach((a, i, arr) => {
+      a !== arr[i - 1] ? numArr.push(2) : numArr[numArr.length - 1]++;
+    });
+  return numArr.reduce((acc, cur) => acc * cur) - 1;
+}
