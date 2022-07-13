96e728fbd63deb888de0be713ef8b0544058195f
KIMMINSIK <we_in@naver.com>
2022-07-13 22:56:41 +0900

    week4 solved

--- 김민식/week4.js
+++ 김민식/week4.js
@@ -1,1 +1,15 @@
-//4주차부터 참여시작하겠습니다.
+function solution(n, words) {
+  let answer = [];
+  let count = 2;
+  let lastWord = words.map((item) => item[item.length - 1]);
+  for (let i = 1; i < lastWord.length; i++) {
+    answer.push(words[i - 1]);
+    if (words[i][0] !== lastWord[i - 1] || answer.includes(words[i])) {
+      return count % n === 0
+        ? [n, Math.ceil(count / n)]
+        : [count % n, Math.ceil(count / n)];
+    }
+    count++;
+  }
+  return [0, 0];
+}
