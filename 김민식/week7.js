9eeeff760bb22b6a0f3053a168a6bc2c50289802
KIMMINSIK <we_in@naver.com>
2022-08-09 22:17:27 +0900

    주석

--- 김민식/week7.js
+++ 김민식/week7.js
@@ -3,7 +3,7 @@ function solution(s) {
   let name = s.replace(/{/g, (e) => "");
   let list = [];
   let arr = name.split("},").map((item) => item.split(","));
-  for (let i = 1; i < c.length + 1; i++) {
+  for (let i = 1; i < arr.length + 1; i++) {
     arr.forEach((item) => {
       if (item.length === i) {
         item.forEach((item) => {
