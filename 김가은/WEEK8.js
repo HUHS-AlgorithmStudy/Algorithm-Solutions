87e4092f386ea162058dcb431e734536d25e587b
KimGaeun0806 <greencloud11@hanyang.ac.kr>
2022-08-13 20:10:32 +0900

    Solved Week 8

---
+++ 김가은/WEEK8.js
@@ -0,0 +1,15 @@
+function solution(w, h) {
+  let width = 0;
+  let heigth = 0;
+  let num = 0;
+
+  for (let i = w; i > 0; i--) {
+    if (h % i === 0 && w % i === 0) {
+      [width, heigth] = [w / i, h / i];
+      num = i;
+      break;
+    }
+  }
+
+  return w * h - num * (width + heigth - 1);
+}
