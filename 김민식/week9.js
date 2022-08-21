67d4cea459ce729a4a46887ad0351adf5c8c3de6
KIMMINSIK <we_in@naver.com>
2022-08-21 17:26:09 +0900

    add comment

--- 김민식/week9.js
+++ 김민식/week9.js
@@ -1,10 +1,8 @@
 // 최종풀이
 function solution(s) {
-  s = s.split("");
   let stack = [s[0]];
   for (let i = 1; i < s.length; i++) {
-    if (stack[stack.length - 1] === s[i]) stack.pop();
-    else stack.push(s[i]);
+    stack[stack.length - 1] === s[i] ? stack.pop() : stack.push(s[i]);
   }
   return stack.length === 0 ? 1 : 0;
 }
